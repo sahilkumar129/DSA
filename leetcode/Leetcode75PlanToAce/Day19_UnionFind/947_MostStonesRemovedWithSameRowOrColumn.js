932bf6b5eef1cc477cb6ed3a156c9e0dd24f09a0
Sahil Kumar <sahil@elocitytech.com>
2022-08-12 13:24:31 +0530

    daily challenge + practice problems

--- leetcode/Leetcode75PlanToAce/Day19_UnionFind/947_MostStonesRemovedWithSameRowOrColumn.js
+++ leetcode/Leetcode75PlanToAce/Day19_UnionFind/947_MostStonesRemovedWithSameRowOrColumn.js
@@ -2,7 +2,80 @@
  * @param {number[][]} stones
  * @return {number}
  */
-var removeStones = function (stones) {};
+// var removeStones = function (stones) {
+//   let ans = 0;
+//   const vis = new Set();
+//   for (let i = 0; i < stones.length; i++) if (!vis.has(i)) ans += dfs(i, stones, vis);
+//   return ans;
+// };
+
+// var dfs = function (ind, stones, vis) {
+//   let count = 0;
+//   vis.add(ind);
+//   for (let i = 0; i < stones.length; i++)
+//     if (!vis.has(i) && (stones[i][0] === stones[ind][0] || stones[i][1] === stones[ind][1]))
+//       count += dfs(i, stones, vis) + 1;
+//   return count;
+// };
+
+class UnionFind {
+  constructor(n) {
+    this.graph = [...Array(n)].map((_, i) => i);
+    this.sizes = new Array(n).fill(0);
+    this.count = n;
+  }
+
+  union(a, b) {
+    let A = this.find(a);
+    let B = this.find(b);
+    if (A === B) return;
+    //weighting optimization part
+    if (this.sizes[A] < this.sizes[B]) {
+      this.graph[A] = B;
+      this.sizes[B] += this.sizes[A];
+    } else {
+      this.graph[B] = A;
+      this.sizes[A] += this.sizes[B];
+    }
+    this.count--;
+  }
+
+  find(id) {
+    let root = id;
+    // finding root
+    while (root !== this.graph[root]) {
+      root = this.graph[root];
+    }
+    // path compression
+    while (id !== root) {
+      const nextId = this.graph[id];
+      this.graph[id] = root;
+      id = nextId;
+    }
+    return root;
+  }
+}
+
+/**
+ * @param {number[][]} stones
+ * @return {number}
+ */
+var removeStones = function (stones) {
+  const sizes = stones.length;
+  const uf = new UnionFind(sizes);
+
+  // if any two points are on the same column or row, they are connected as a
+  // edge.
+  // count the number of disjoint components (all - connected).
+  for (let i = 0; i < sizes; i++) {
+    for (let j = i + 1; j < sizes; j++) {
+      if (stones[i][0] === stones[j][0] || stones[i][1] === stones[j][1]) {
+        uf.union(i, j);
+      }
+    }
+  }
+  return sizes - uf.count;
+};
 
 const stones = [
   [0, 0],
